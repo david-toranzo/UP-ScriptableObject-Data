7c0e05d7dd1371587c735d5fe92b97dd36d9fdf1
david444456 <david-toranzo-@hotmail.com>
2023-04-06 20:06:03 -0300

    [A] add data event vector 3

---
+++ Runtime/Data/IntDataScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "IntDataSO", menuName = "ScriptableObjects/Data/Int", order = 0)]
+    public class IntDataScriptableObject : TypeDataScriptableObject<int>
+    {
+    
+    }
+}
---
+++ Runtime/Data/Vector3DataScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "Vector3DataSO", menuName = "ScriptableObjects/Data/Vector3", order = 0)]
+    public class Vector3DataScriptableObject : TypeDataScriptableObject<Vector3>
+    {
+
+    }
+}
