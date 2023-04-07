e055b72012240d5cafefecb694928ed0be870de5
david444456 <david-toranzo-@hotmail.com>
2023-04-07 18:11:02 -0300

    [A] add array data object

---
+++ Runtime/Data/BoolArrayDataScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "BoolArrayDataSO", menuName = "ScriptableObjects/Data/BoolArray", order = 0)]
+    public class BoolArrayDataScriptableObject : TypeDataScriptableObject<bool[]>
+    {
+
+    }
+}
