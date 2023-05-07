4e5aec72972934bf7f77ccaaeeec804e1e28c3d9
david444456 <david-toranzo-@hotmail.com>
2023-05-07 14:29:19 -0300

    [A] add bool data so

---
+++ Runtime/Data/BoolDataScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "BoolDataSO", menuName = "ScriptableObjects/Data/Bool", order = 0)]
+    public class BoolDataScriptableObject : TypeDataScriptableObject<bool>
+    {
+
+    }
+}
